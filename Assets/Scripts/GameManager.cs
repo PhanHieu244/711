db9e3f1bf53cb450cd5d36d78b9465fe127ee583
PhanHieu244 <phanhieu2443@gmail.com>
2024-08-28 19:55:34 +0700

    init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int playerRank;
+    public bool playerCrownActivity;
+    public List<Transform> playerPositions;
+
+
+    [Header("Panels")]
+    public GameObject generalGroup;
+    public GameObject LevelComplete;
+    public GameObject LevelFail;
+
+
+   
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    //public void GeneralPanelOff()
+    //{
+    //    generalGroup.SetActive(false);
+    //}
+
+    public void LevelCompletePanelOn()
+    {
+        StartCoroutine(LevelCompleteDelay());
+    }
+
+    public void LevelFailPanelOn()
+    {
+        StartCoroutine(LevelFailDelay());
+    }
+
+    public void PlayerRankIncrement()
+    {
+        playerRank++;
+    }
+
+    public void PlayerCrownActive()
+    {
+        playerCrownActivity = true;
+    }
+
+    public void PlayerCrownNotActive()
+    {
+        playerCrownActivity = false;
+    }
+
+    IEnumerator LevelCompleteDelay()
+    {
+        yield return new WaitForSeconds(2); 
+        generalGroup.SetActive(false);
+        LevelComplete.SetActive(true);
+    }
+
+    IEnumerator LevelFailDelay()
+    {
+        yield return new WaitForSeconds(2);
+        generalGroup.SetActive(false);
+        LevelFail.SetActive(true);
+    }
+}
